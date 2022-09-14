8084031f892683282484d5a924ec7ad7d5bd7992
Newton Rhomel Beck Angelini <newton.beck@personio.de>
2022-09-14 20:39:50 +0200

    Saves state in the blockchain

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,8 +6,27 @@ import "hardhat/console.sol";
 
 contract WavePortal {
 
+    mapping(address => uint256) waves;
+
+    uint256 totalWaves;
+
     constructor() {
         console.log("Yo yo, I am a smart contract and I am smart");
+    }
+
+    function wave() public {
+        waves[msg.sender]++;
+        totalWaves++;
+        console.log("%s has waved", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves", totalWaves);
+        return totalWaves;
+    }
+
+    function getMyTotalWaves() public view returns (uint256) {
+        return waves[msg.sender];
     }
 
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,12 +1,31 @@
 const { ethers } = require("hardhat");
 
 const main = async () => {
+    const [owner, randomPerson] = await ethers.getSigners();
     const contractFactory = await ethers.getContractFactory("WavePortal");
     const wavePortalContract = await contractFactory.deploy();
 
     await wavePortalContract.deployed();
 
     console.log("Contract deployed to:", wavePortalContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    let waveCount;
+
+    waveCount = await wavePortalContract.getTotalWaves();
+    console.log("Total wave count before waving:", waveCount);
+
+    const waveTransactionOne = await wavePortalContract.wave();
+    await waveTransactionOne.wait();
+
+    const waveTransactionTwo = await wavePortalContract.connect(randomPerson).wave();
+    await waveTransactionTwo.wait();
+
+    waveCount = await wavePortalContract.getTotalWaves();
+    console.log("Total wave count after waving:", waveCount);
+
+    console.log(`Total waves of ${owner.address} is`, await wavePortalContract.getMyTotalWaves());
+    console.log(`Total waves of ${randomPerson.address} is`, await wavePortalContract.connect(randomPerson).getMyTotalWaves());
 };
 
 main()
