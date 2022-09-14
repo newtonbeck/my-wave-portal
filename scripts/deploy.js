58071b61c6129175714e9d023ea932f6e3ecc4bf
Newton Rhomel Beck Angelini <newton.beck@personio.de>
2022-09-14 20:57:29 +0200

    Adds the deployment script to the project

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+const { ethers } = require("hardhat");
+
+const main = async () => {
+    const contractFactory = await ethers.getContractFactory("WavePortal");
+    const wavePortal = await contractFactory.deploy();
+
+    await wavePortal.deployed();
+
+    console.log("Contract deployed at", wavePortal.address);
+};
+
+main()
+    .then(() => {
+        process.exit(0);
+    })
+    .catch((e) => {
+        console.error(e);
+        process.exit(1);
+    });
