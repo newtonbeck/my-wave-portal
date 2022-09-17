c3fc5011808ee65c902846b4c841e2a0fff16355
Newton Rhomel Beck Angelini <newton.beck@personio.de>
2022-09-17 07:44:03 +0200

    Funds the contract while deploying it

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,9 @@ const { ethers } = require("hardhat");
 
 const main = async () => {
     const contractFactory = await ethers.getContractFactory("WavePortal");
-    const wavePortal = await contractFactory.deploy();
+    const wavePortal = await contractFactory.deploy({
+        value: ethers.utils.parseEther("0.001"),
+    });
 
     await wavePortal.deployed();
 
